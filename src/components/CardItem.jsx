e2948a323d304598e4e45454381f89517f00cdd3
AnubhavSS <127021873+AnubhavSS@users.noreply.github.com>
2023-06-02 16:25:37 +0530

    Update CardItem.jsx

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -7,7 +7,7 @@ const CardItem = ({item}) => {
     <div > 
         
         <div className="card  mx-auto  my-2 bg-body-secondary overflow-hidden " style={{width: '18rem',height: '460px' }}>
-  <img src={item.show.image.original} className="card-img-top img-fluid " style={{height:'30vh'}} alt="..."/>
+  <img src={item.show.image.original} className="card-img-top img-fluid  h-50 h-lg-75 " alt="..."/>
   <div className="card-body  h-100 bg-opacity-25">
     <h5 className="card-title">{item.show.name}<span className='fst-italic fs-6'>({item.show.language})</span></h5>
     <p className="card-text mb-4 fw-semibold">{item.show.summary===""? <span>No description available</span>: item.show.summary.substring(0,100)}..</p>
